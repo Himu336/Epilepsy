17c155c57ce0b94fd9b015c7ee0dbbe1ae354304
Himansh <himanshbansal200521@gmail.com>
2025-06-15 21:52:57 +0530

    error

--- src/repositories/checklist-repository.js
+++ src/repositories/checklist-repository.js
@@ -1,16 +1,33 @@
-const { SymptomChecklist } = require('../models');
+const { SymptomChecklist, Patient } = require('../models');
 
 class ChecklistRepository {
   async create(data) {
-    return await SymptomChecklist.create(data);
+    const checklist = await SymptomChecklist.create(data);
+    return await SymptomChecklist.findByPk(checklist.id, {
+      include: [{
+        model: Patient,
+        attributes: ['id', 'name']
+      }]
+    });
   }
 
   async findAllByPatientId(patient_id) {
-    return await SymptomChecklist.findAll({ where: { patient_id } });
+    return await SymptomChecklist.findAll({
+      where: { patient_id },
+      include: [{
+        model: Patient,
+        attributes: ['id', 'name']
+      }]
+    });
   }
 
   async findById(id) {
-    return await SymptomChecklist.findByPk(id);
+    return await SymptomChecklist.findByPk(id, {
+      include: [{
+        model: Patient,
+        attributes: ['id', 'name']
+      }]
+    });
   }
 }
 
